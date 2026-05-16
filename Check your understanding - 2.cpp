d5a6efbc198426d5b0e02889525657e016ea1392
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-16 15:44:13 +0530

    Add age classification exercise

---
+++ Check your understanding - 2.cpp
@@ -0,0 +1,27 @@
+// Given someone’s age, tell whether they are a child, adult,
+// or a senior citizen.
+// 0-17 : Child
+// 18-64 : Adult
+// 65+ : Senior Citizen
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter age : ";
+    cin >> age;
+
+    if (age >= 0 && age <= 17) {
+        cout << "Child";
+    } else if (age >= 18 && age <= 64) {
+        cout << "Adult";
+    }
+
+    else {
+        cout << "Senior Citizen";
+    }
+}
+
+// 2. Take input of 3 numbers x, y, z and output the maximum
+// using if statements
