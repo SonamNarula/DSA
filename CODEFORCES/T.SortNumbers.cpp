a4680fe65e17590384f9b842fadb1eb2b3fe4b3e
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-16 17:06:52 +0530

    Add Codeforces sort numbers solution

---
+++ CODEFORCES/T.SortNumbers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    int x = A, y = B, z = C;
+
+    if (x > y) {
+        swap(x, y);
+    }
+
+    if (x > z) {
+        swap(x, z);
+    }
+
+    if (y > z) {
+        swap(y, z);
+    }
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+
+    cout << endl;
+
+    cout << A << endl;
+    cout << B << endl;
+    cout << C << endl;
+}
