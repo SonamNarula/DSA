2f1c9c8cd3c868b60eea10d0934bc066385c6a52
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-17 18:13:07 +0530

    Update Codeforces solutions

---
+++ CODEFORCES SHEET 1/A.SayHelloWithC++.cpp
@@ -0,0 +1,8 @@
+// Given a name S. Print "Hello, (name)" without parentheses.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+}
--- CODEFORCES/F.DigitsSummation.cpp
+++ CODEFORCES/F.DigitsSummation.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // Changed 'int' to 'long long' to handle numbers up to 10^18
-    long long a, b; 
+    long long a, b;
     cin >> a >> b;
 
     cout << ((a % 10) + (b % 10));
