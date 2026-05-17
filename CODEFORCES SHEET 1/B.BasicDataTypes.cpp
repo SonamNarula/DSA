3bac348f869550e3d6790110b2464b1ddb27cedd
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-17 18:47:27 +0530

    Add basic data types solution

---
+++ CODEFORCES SHEET 1/B.BasicDataTypes.cpp
@@ -0,0 +1,26 @@
+// Input
+// Only one line containing the following space-separated values: int, long long, char, float and double respectively.
+
+// Output
+// Print each element on a new line in the same order it was received as input.
+
+// Don't print any extra spaces.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+}
