8a1f2f3eb24291120914694370f17cbb337119ba
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-17 18:15:15 +0530

    Complete hello solution

--- CODEFORCES SHEET 1/A.SayHelloWithC++.cpp
+++ CODEFORCES SHEET 1/A.SayHelloWithC++.cpp
@@ -1,11 +1,11 @@
 // Given a name S. Print "Hello, (name)" without parentheses.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     string s;
     cin >> s;
 
-    cout << "Hello, " << s
+    cout << "Hello, " << s;
 }
