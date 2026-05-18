85a0c667026764bb7e211a754b31674de54f3b57
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-18 17:16:00 +0530

    Add welcome conditions solution

---
+++ CODEFORCES SHEET 1/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,21 @@
+// Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
+
+// Input
+// Only one line containing two numbers A and B (0  ≤  A, B  ≤  100).
+
+// Output
+// Print "Yes" or "No" according to the statement.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    if (A >= B) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+}
