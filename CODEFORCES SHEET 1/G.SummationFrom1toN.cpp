92818fa65e04e7b1adb80b0c4188a77d6d87c405
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-18 16:01:38 +0530

    Update summation problem statement

---
+++ CODEFORCES SHEET 1/G.SummationFrom1toN.cpp
@@ -0,0 +1,12 @@
+// Given a number 𝑁
+// . Print the summation of the numbers that is between 1 and 𝑁
+//  (inclusive).
+
+// .∑𝑖=1𝑁𝑖
+// Input
+// Only one line containing a number 𝑁
+//  (1≤𝑁≤109)
+
+// Output
+// Print the summation of the numbers that are between 1 and 𝑁
+//  (inclusive).
