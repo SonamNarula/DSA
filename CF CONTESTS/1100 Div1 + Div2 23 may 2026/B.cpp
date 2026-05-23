2b33a1126fbef8a4233ceca2ae53f870a4c08e1e
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-23 20:34:28 +0530

    problem b of the contest

--- CF CONTESTS/1100 Div1 + Div2 23 may 2026/B.cpp
+++ CF CONTESTS/1100 Div1 + Div2 23 may 2026/B.cpp
@@ -32,3 +32,29 @@
 // Output
 
 // For each testcase, output the maximum value of max(𝑎)+∑𝑛𝑖=1𝑏𝑖 attainable.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long long> a(n), b(n);
+        for (auto& x : a) cin >> x;
+        for (auto& x : b) cin >> x;
+        
+        long long sumMax = 0, maxMin = 0;
+        for (int i = 0; i < n; i++) {
+            sumMax += max(a[i], b[i]);
+            maxMin = max(maxMin, min(a[i], b[i]));
+        }
+        cout << sumMax + maxMin << "\n";
+    }
+    return 0;
+}
