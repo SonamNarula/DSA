5bd519805b4949e3eeb32bb06709b69af5709fdd
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-27 10:55:00 +0530

    oop

--- OOP/oop.cpp
+++ OOP/oop.cpp
@@ -3,15 +3,24 @@
 using namespace std;
 
 class Teacher {
-public:
+   public:
     // man made constructor
+    // non parameterised
     Teacher() { dept = "Computer Science"; }
     // properties/attributes
 
-private:
+    // parameterised constructor
+    Teacher(string n, string s, string d, double sal) {
+        name = n;
+        subject = s;
+        dept = d;
+        salary = sal;
+    }
+
+   private:
     double salary = 0;
 
-public:
+   public:
     string name;
     string dept;
     string subject;
@@ -23,33 +32,46 @@ public:
 
     // getter
     double getSalary() const { return salary; }
+
+    void getInfo(){
+        cout << "Name : " << name << endl;
+        cout <<"Subject : " << subject << endl;
+    }
 };
 
 class Account {
-private:
+   private:
     // data hiding
     double balance = 0;
     string password;
 
-public:
+   public:
     string accountId;
     string username;
 
     void setBalance(double b) { balance = b; }
     double getBalance() const { return balance; }
+
+    // void getInfo() const {
+    //     cout << "Account ID : " << accountId << endl;
+    //     cout << "Username : " << username << endl;
+    // }
 };
 
 int main() {
     // object
-    Teacher t1;  // constructor call
-    t1.name = "sonam";
-    t1.subject = "C++";
-    // t1.dept = "CSE";
-    t1.setSalary(20000);
+    Teacher t1("Sonam", "c++", "cse", 20000);  // constructor call
+    // t1.getInfo();
+    Teacher t2(t1);
+    t2.getInfo();
+    // t1.name = "sonam";
+    // t1.subject = "C++";
+    // // t1.dept = "CSE";
+    // t1.setSalary(20000);
 
-    cout << t1.name << endl;
-    cout << t1.getSalary() << endl;
-    cout << t1.dept;
+    // cout << t1.name << endl;
+    // cout << t1.getSalary() << endl;
+    // cout << t1.dept << endl;
 
     return 0;
 }
