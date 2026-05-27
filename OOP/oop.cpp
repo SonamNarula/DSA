2ea994180176109679f23cd116b3989f018898d4
Sonam Narula <sonamnarula2108@gmail.com>
2026-05-27 10:03:41 +0530

    oop

--- OOP/oop.cpp
+++ OOP/oop.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 class Teacher {
+public:
+    // man made constructor
+    Teacher() { dept = "Computer Science"; }
     // properties/attributes
 
-   private:
-    double salary;
+private:
+    double salary = 0;
 
-   public:
+public:
     string name;
     string dept;
     string subject;
@@ -19,19 +22,34 @@ class Teacher {
     void setSalary(double s) { salary = s; }
 
     // getter
-    double getSalary() { return salary; }
+    double getSalary() const { return salary; }
+};
+
+class Account {
+private:
+    // data hiding
+    double balance = 0;
+    string password;
+
+public:
+    string accountId;
+    string username;
+
+    void setBalance(double b) { balance = b; }
+    double getBalance() const { return balance; }
 };
 
 int main() {
     // object
-    Teacher t1;
+    Teacher t1;  // constructor call
     t1.name = "sonam";
     t1.subject = "C++";
-    t1.dept = "CSE";
+    // t1.dept = "CSE";
     t1.setSalary(20000);
 
     cout << t1.name << endl;
     cout << t1.getSalary() << endl;
+    cout << t1.dept;
 
     return 0;
 }
