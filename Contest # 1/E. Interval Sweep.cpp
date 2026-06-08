93641395de94d02acc8acef242c030954c1ff8ed
Sonam Narula <sonamnarula2108@gmail.com>
2026-06-08 19:38:21 +0530

    contest sheet 1

---
+++ Contest # 1/E. Interval Sweep.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(a + b > 0 && abs(a - b) <= 1)
+        cout << "YES";
+    else
+        cout << "NO";
+}
