a2b961bd98adf8892a1cec1caaadf9ef7c6df197
Sonam Narula <sonamnarula2108@gmail.com>
2026-06-09 07:36:21 +0530

    cf sheet 2

--- CODEFORCES SHEET 2/B. Even Numbers.cpp
+++ CODEFORCES SHEET 2/B. Even Numbers.cpp
@@ -5,6 +5,11 @@ int main(){
     int n;
     cin >> n;
 
+     if(n < 2) {
+        cout << -1;
+        return 0;
+    }
+
     for(int i = 1; i <= n; i++){
         if(i%2==0)
         cout<<i<<endl;
