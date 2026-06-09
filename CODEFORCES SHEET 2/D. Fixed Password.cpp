9fb11a1b41ed7ba6ef8f8ffd8fdc389f066d40bb
Sonam Narula <sonamnarula2108@gmail.com>
2026-06-09 07:45:44 +0530

    cf sheet 2

---
+++ CODEFORCES SHEET 2/D. Fixed Password.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int x;
+
+    while (cin >> x) {
+
+        if (x == 1999) {
+            cout << "Correct\n";
+            break;
+        }
+
+        cout << "Wrong\n";
+    }
+
+    return 0;
+}
