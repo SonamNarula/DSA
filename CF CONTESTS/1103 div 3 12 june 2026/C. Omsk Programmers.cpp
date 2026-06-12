182b001d7bb39974254f376b811416687fc829be
Sonam Narula <sonamnarula2108@gmail.com>
2026-06-12 21:13:03 +0530

    div 3 contest on 12 june 2026

--- CF CONTESTS/1103 div 3 12 june 2026/C. Omsk Programmers.cpp
+++ CF CONTESTS/1103 div 3 12 june 2026/C. Omsk Programmers.cpp
@@ -1,89 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-// learning
 using namespace std;
 
-
-long long get_prod(long long v, long long x, int p, long long limit) {
-    long long prod = v;
-    for (int i = 0; i < p; i++) {
-        if (prod > limit / x) {
-            return limit + 1; 
-        }
-        prod *= x;
-    }
-    return prod;
-}
-
-void solve() {
-    long long a, b, x;
-    cin >> a >> b >> x;
-    
-
-    vector<long long> candidates;
-    
-    long long cur_a = a;
-    candidates.push_back(cur_a);
-    while (cur_a > 0) {
-        cur_a /= x;
-        candidates.push_back(cur_a);
-    }
-    
-    long long cur_b = b;
-    candidates.push_back(cur_b);
-    while (cur_b > 0) {
-        cur_b /= x;
-        candidates.push_back(cur_b);
-    }
-    
-
-    sort(candidates.begin(), candidates.end());
-    candidates.erase(unique(candidates.begin(), candidates.end()), candidates.end());
-    
-    long long ans = 1e18; 
-    for (long long v : candidates) {
-        
-
-        long long cost_a = 1e18;
-        for (int p = 0; p <= 32; p++) {
-            long long div_val = a;
-            for (int i = 0; i < p; i++) {
-                div_val /= x;
-            }
-
-            if (v >= div_val) {
-                long long prod = get_prod(v, x, p, 2e9);
-                long long additions = max(0LL, prod - a);
-                cost_a = min(cost_a, p + additions);
-            }
-        }
-        
-
-        long long cost_b = 1e18;
-        for (int q = 0; q <= 32; q++) {
-            long long div_val = b;
-            for (int i = 0; i < q; i++) {
-                div_val /= x;
-            }
-            if (v >= div_val) {
-                long long prod = get_prod(v, x, q, 2e9);
-                long long additions = max(0LL, prod - b);
-                cost_b = min(cost_b, q + additions);
-            }
-        }
-        
-        ans = min(ans, cost_a + cost_b);
-    }
-    
-    cout << ans << "\n";
-}
-
-int main() {
+int main(){
     int t;
-    cin >> t;
-    while (t--) {
-        solve();
+    scanf("%d", &t);
+    while(t--){
+        long long a, b, x;
+        scanf("%lld %lld %lld", &a, &b, &x);
+
+        vector<long long> va, vb;
+
+        long long cur = a;
+        va.push_back(cur);
+        while(cur != 0){
+            cur /= x;
+            va.push_back(cur);
+        }
+
+        cur = b;
+        vb.push_back(cur);
+        while(cur != 0){
+            cur /= x;
+            vb.push_back(cur);
+        }
+
+        long long ans = LLONG_MAX;
+        for(int i = 0; i < (int)va.size(); i++){
+            for(int j = 0; j < (int)vb.size(); j++){
+                long long cost = i + j + abs(va[i] - vb[j]);
+                ans = min(ans, cost);
+            }
+        }
+
+        printf("%lld\n", ans);
     }
-    return 0;
 }
