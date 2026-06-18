36314fd05c340673c830cc57175751fe167ab90e
Sonam Narula <sonamnarula2108@gmail.com>
2026-06-18 09:53:42 +0530

    max sum subarray of size k

--- SLIDING WINDOW/Max Sum Subarray of size K.cpp
+++ SLIDING WINDOW/Max Sum Subarray of size K.cpp
@@ -1,24 +1,24 @@
 class Solution {
-  public:
+   public:
     int maxSubarraySum(vector<int>& arr, int k) {
         // code here
         int WindowSum = 0;
         int n = arr.size();
-        
+
         // first window
-        for (int i = 0; i < k; i++){
-            WindowSum+=arr[i];
+        for (int i = 0; i < k; i++) {
+            WindowSum += arr[i];
         }
         int maxSum = WindowSum;
-        
+
         // slide the window
-        for(int right = k; right<n;right++){
+        for (int right = k; right < n; right++) {
             int left = right - k;
-            
+
             WindowSum += arr[right];
             WindowSum -= arr[left];
-            
-            maxSum = max(maxSum,WindowSum);
+
+            maxSum = max(maxSum, WindowSum);
         }
         return maxSum;
     }
