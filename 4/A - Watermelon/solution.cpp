01674088c2496081e37e45a219b1cc46fd189521
Sonam Narula <sonamnarula2108@gmail.com>
2026-07-21 17:11:14 +0530

    Add Watermelon [A] from Codeforces

--- 4/A - Watermelon/solution.cpp
+++ 4/A - Watermelon/solution.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
- 
+ 
 int main() {
-    int w;
-    cin >> w;
- 
-    if (w > 2 && w % 2 == 0)
+    int wt;
+    cin >> wt;
+ 
+    if (wt > 2 && wt % 2 == 0)
         cout << "YES";
     else
         cout << "NO";
- 
+ 
     return 0;
 }
