0db8a2c32cf6062fdfb556bfa479ab7fbac104ca
Sonam Narula <sonamnarula2108@gmail.com>
2026-07-23 16:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (15.90%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(), s.end());
+
+        // method 1 : stl
+        // reverse(s.begin(), s.end());
+
+
+        // method 2 : 2 pointers
+
+        int start = 0; int end = s.size() - 1;
+
+        while(start<end){
+            swap(s[start], s[end]);
+            start++;
+            end--;
+        }
     }
 };
