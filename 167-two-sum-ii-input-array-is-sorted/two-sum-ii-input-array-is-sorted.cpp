1ab36f089ced3a57a0b905e705d401584cb2e4ba
Sonam Narula <sonamnarula2108@gmail.com>
2026-07-29 08:25:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (95.66%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,21 +1,40 @@
 class Solution {
+
 public:
+
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int left = 0;
-        int right = numbers.size() - 1;
 
-        while (left < right) {
-            int sum = numbers[left] + numbers[right];
+        int n = numbers.size();
 
-            if (sum == target)
-                return {left + 1, right + 1};
+        for(int i = 0; i < n; i++) {
 
-            if (sum > target)
-                --right;
-            else
-                ++left;
+            int complement = target - numbers[i];
+
+            int left = i + 1;
+
+            int right = n - 1;
+
+            while(left <= right) {
+
+                int mid = left + (right - left) / 2;
+
+                if(numbers[mid] == complement)
+
+                    return {i + 1, mid + 1};
+
+                else if(numbers[mid] < complement)
+
+                    left = mid + 1;
+
+                else
+
+                    right = mid - 1;
+
+            }
+
         }
 
         return {};
+
     }
 };
