d492f8b7d2393bddef59dee4cecdbb24b1f82962
Sonam Narula <sonamnarula2108@gmail.com>
2026-07-29 11:03:57 +0530

    Time: 336 ms (21.21%) | Memory: 0B (100.00%) - LeetSync

---
+++ 595-big-countries/big-countries.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+ -- Write your MySQL query statement below
+
+SELECT name, population, area
+FROM World
+WHERE population >= 25000000  OR area >= 3000000;
+
+-- UNION 
+
+-- SELECT name, population, area
+-- FROM World
+-- WHERE 
