2ff14a9a25f06c7f577495ad78b4d13ffa9d4686
Sonam Narula <sonamnarula2108@gmail.com>
2026-07-30 20:02:16 +0530

    Time: 1217 ms (15.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,11 @@
+-- 1633. Percentage of Users Attended a Contest
+SELECT
+    r.contest_id,
+    ROUND(
+        COUNT(r.user_id) * 100.0 /
+        (SELECT COUNT(*) FROM Users),
+        2
+    ) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, contest_id ASC;
