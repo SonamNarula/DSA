cb60274bd804420a16a7c31389e43f2478e5af09
Sonam Narula <sonamnarula2108@gmail.com>
2026-07-30 20:03:56 +0530

    Time: 527 ms (60.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+-- 2356. Number of Unique Subjects Taught by Each Teacher
+SELECT
+    teacher_id,
+    COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id
+ORDER BY teacher_id;
