971ab09292557aaa43dc860fbedc24bc271ff8fc
Sonam Narula <sonamnarula2108@gmail.com>
2026-08-10 04:32:00 +0530

    Time: 9 ms (61.36%) | Memory: 13.8 MB (43.69%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+
+        vector<int> suf(n + 1, 0);
+        for (int i = n - 1; i >= 0; --i)
+            suf[i] = suf[i + 1] + piles[i];
+
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int m = n; m >= 1; --m) {
+                if (i + 2 * m >= n) {
+                    dp[i][m] = suf[i];
+                    continue;
+                }
+
+                for (int x = 1; x <= 2 * m; ++x) {
+                    dp[i][m] = max(
+                        dp[i][m],
+                        suf[i] - dp[i + x][max(m, x)]
+                    );
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
+};
