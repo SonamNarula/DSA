ebc651052ff3626f93be6acc053430de41197ce1
Sonam Narula <sonamnarula2108@gmail.com>
2026-08-25 18:37:33 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (38.66%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,15 +1,46 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result;
 
-        for(int i = 0; i < n; i++) {
-            result.push_back(nums[i] * nums[i]);
+        int n = nums.size();
+
+        // Answer array ka size same hoga as nums
+        vector<int> ans(n);
+
+        // 2 pointers:
+        // left -> array ke starting element par
+        // right -> array ke last element par
+        int left = 0;
+        int right = n - 1;
+
+        // Hum answer ko RIGHT se LEFT fill karenge
+        // Kyunki humein sabse bada square pehle mil raha hai
+        for(int i = n - 1; i >= 0; i--) {
+
+            // Left aur right dono ends ke elements ka
+            // absolute value compare karo
+            if(abs(nums[left]) > abs(nums[right])) {
+
+                // Left wala bada hai
+                // Uska square answer ke current last position par daalo
+                ans[i] = nums[left] * nums[left];
+
+                // Left pointer ko aage badhao
+                left++;
+            }
+
+            else {
+
+                // Right wala bada hai
+                // Uska square answer ke current last position par daalo
+                ans[i] = nums[right] * nums[right];
+
+                // Right pointer ko peeche lao
+                right--;
+            }
         }
 
-        sort(result.begin(), result.end());
-
-        return result;
+        // Sorted squares return karo
+        return ans;
     }
 };
