980165277d495fa8906fabc90595fe039ab0a837
Sonam Narula <sonamnarula2108@gmail.com>
2026-09-09 13:57:56 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (14.94%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
 
-        int bestEnding = nums[0];
+        // Best subarray ending at index 0
+        int curr = nums[0];
+
+        // Best answer anywhere
         int ans = nums[0];
 
-        for (int i = 1; i < n; i++) {
-            int v1 = bestEnding + nums[i];
-            int v2 = nums[i];
+        for (int i = 1; i < nums.size(); i++) {
 
-            bestEnding = max(v1, v2);
-            ans = max(ans, bestEnding);
+            // Start fresh OR extend previous subarray
+            curr = max(nums[i], curr + nums[i]);
+
+            // Update global maximum
+            ans = max(ans, curr);
         }
 
         return ans;
