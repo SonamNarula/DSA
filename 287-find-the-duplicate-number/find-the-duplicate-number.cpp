fb576cd8d4923a9f15c504fb3d9e2b177aae9495
Sonam Narula <sonamnarula2108@gmail.com>
2026-09-09 12:21:44 +0530

    Time: 7 ms (50.26%) | Memory: 64.9 MB (90.89%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
-        // --------------------------------------------------
-        // STEP 1: Find the meeting point of slow and fast
-        // --------------------------------------------------
-        
+
+        // Phase 1: Find meeting point
         int slow = nums[0];
         int fast = nums[0];
 
         do {
-            // Slow pointer moves one step
+
             slow = nums[slow];
 
-            // Fast pointer moves two steps
             fast = nums[nums[fast]];
 
         } while (slow != fast);
 
+        // Phase 2: Find cycle entrance
+        slow = nums[0];
 
-        // --------------------------------------------------
-        // STEP 2: Find the entrance of the cycle
-        // --------------------------------------------------
-        // At this point, slow and fast are meeting somewhere
-        // inside the cycle.
-        //
-        // Now start another pointer from the beginning.
-        // Move both pointers one step at a time.
-        // The point where they meet is the duplicate number.
-        // --------------------------------------------------
+        while (slow != fast) {
 
-        int slow2 = nums[0];
-
-        while (slow2 != slow) {
-            
-            // Move both pointers one step
-            slow2 = nums[slow2];
             slow = nums[slow];
+            fast = nums[fast];
         }
 
-        // The meeting point is the duplicate number
         return slow;
     }
 };
