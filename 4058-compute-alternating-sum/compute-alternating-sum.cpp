2d32cb783b85978ae68a1df16fbf104e8f18d145
Sonam Narula <sonamnarula2108@gmail.com>
2026-09-11 19:17:05 +0530

    Time: 0 ms (100.00%) | Memory: 32.4 MB (79.47%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            
+            if (i % 2 == 0) {
+                sum += nums[i];   // even index → +
+            } 
+            else {
+                sum -= nums[i];   // odd index → -
+            }
+        }
+
+        return sum;
+    }
+};
